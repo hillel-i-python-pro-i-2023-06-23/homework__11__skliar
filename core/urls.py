c84190920a7fffc93f84f6e84aef92782d34399c
nick <nikoladarkangel@gmail.com>
2023-07-29 01:55:49 +0300

    fix commit

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
-from applications.base import views as base_views
+
+# from applications.base import views as base_views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', include('applications.base.urls')),
+    path("", include("applications.base.urls")),
 ]
